98352a2637e1e5691d862dc77a7601576ede562e
Jeroen <jeroen@Spartan.(none)>
2015-11-17 16:00:36 +0100

    Receives POST data

--- api.py
+++ api.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	return "Hello World!"
+
+# Adds a pulse measurement
+@app.route("/measurement/pulse", methods=["POST"])
+def add_pulse_measurement():
+	return request.form["test"]
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
