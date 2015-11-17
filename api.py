95b32c80d18f857c54ec11568cd91d0351cc3be6
Jeroen <jeroen@Spartan.(none)>
2015-11-17 16:31:16 +0100

    Adds MyHealth service class

--- api.py
+++ api.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask import request
+from myhealth import MyHealth
+
 app = Flask(__name__)
+service = MyHealth()
 
-@app.route("/")
-def hello():
-	return "Hello World!"
+# Status Codes:
+notfound = 404
+invalid = 402
+ok = 200
 
-# Adds a pulse measurement
+
+### Adds a pulse measurement ###
 @app.route("/measurement/pulse", methods=["POST"])
 def add_pulse_measurement():
-	return request.form["test"]
+	if service.pulse_add(request.form["data"]):
+		return "pulse measurement added", ok
+	else:
+		return "unable to add pulse measurement", invalid
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
---
+++ myhealth.py
@@ -0,0 +1,3 @@
+class MyHealth(object):
+	def pulse_add(self, pulse_json):
+		return True
