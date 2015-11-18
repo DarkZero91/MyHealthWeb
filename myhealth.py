82dfde45cb4038d8df5f88d8cf8762502385064c
Jeroen <jeroen@Spartan.(none)>
2015-11-18 14:52:02 +0100

    Implements store pulse data

--- myhealth.py
+++ myhealth.py
@@ -1,3 +1,17 @@
+import json
+from models.pulse import Pulse
+from persistence.pulsedao import PulseDao
+
 class MyHealth(object):
 	def pulse_add(self, pulse_json):
+		data = json.loads(pulse_json)
+
+		pulse = Pulse(
+			data["id"],
+			data["heartrate"],
+			data["timestamp"])
+		
+		dao = PulseDao.get_instance()
+		dao.save(pulse)
+
 		return True
